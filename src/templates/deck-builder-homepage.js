22a700442fb778af98b52ba6bbe1cda09270bca0
Scott Lewis <scottiemlewis1991@gmail.com>
2019-05-29 16:32:29 -0600

    Maybe this time

---
+++ src/templates/deck-builder-homepage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const DeckBuilderHomepageTemplate = () => {
+    return (
+        <>
+            <span> HOLLLLLYYYYY SHIIIIITTTTT </span>
+        </>
+    );
+};
+
+const DeckBuilderHomepagePage = () => {
+    return (
+        <DeckBuilderHomepageTemplate/>
+    );
+};
+
+export default DeckBuilderHomepagePage;

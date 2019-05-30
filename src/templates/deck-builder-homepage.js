659514b6fe3cabddbee9604cf8e27078ce9dd282
Scott Lewis <scottiemlewis1991@gmail.com>
2019-05-29 18:45:30 -0600

    Test

--- src/templates/deck-builder-homepage.js
+++ src/templates/deck-builder-homepage.js
@@ -1,17 +1,34 @@
 import React from "react";
+import { graphql } from "gatsby";
 
 export const DeckBuilderHomepageTemplate = () => {
     return (
-        <>
+        <React.Fragment>
             <span> HOLLLLLYYYYY SHIIIIITTTTT </span>
-        </>
+        </React.Fragment>>
     );
 };
 
-const DeckBuilderHomepagePage = () => {
+const DeckBuilderHomepagePage = ({ data }) => {
+    const { mardownRemark: page, footerData, navbarData } = data;
+
     return (
-        <DeckBuilderHomepageTemplate/>
+        <Layout footerData={footerData} navbarData={navbarData}>
+            <DeckBuilderHomepageTemplate/>
+        </Layout>
     );
 };
 
 export default DeckBuilderHomepagePage;
+
+export const dbQuery = graphql`
+    query DeckBuilderHomepagePage($id: String!){
+        mardownRemark(id: { eq: $id }){
+            html
+            frontmatter {
+                title
+            }
+        }
+        ...LayoutFragment
+    }
+`;
